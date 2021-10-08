0495937cf9249ce596c869468077899a54ff449a
Hadji, Nadir <hadji.nadir@courrier.uqam.ca>
2021-10-07 20:26:56 -0400

    Modification de la classe principale et de ca méthode main

--- src/Principale.java
+++ src/Principale.java
@@ -1,16 +1,39 @@
 import Entite.Activite;
 import Entite.DeclarationJSON;
+import Entite.General;
+import Entite.Reponse;
+import Service.ServiceValidationDeclaration;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
 
 public class Principale {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) {
+        verifierSiArgumentExiste(args);
+        DeclarationJSON declarationJSON = new DeclarationJSON(args[0], args[1]);
+        charger(declarationJSON);
+        Reponse reponse = new Reponse();
+        General general = new General(declarationJSON);
+        ServiceValidationDeclaration service = new ServiceValidationDeclaration();
+        service.verifierDeclaration(general,reponse);
+        reponse.ecrireFichierDeSortie();
+    }
+
+    private static void verifierSiArgumentExiste(String[] args) {
         if (args.length < 2) {
             System.out.println("Argument invalide");
             System.exit(1);
         }
-        DeclarationJSON json = new DeclarationJSON(args[0], args[1]);
-        json.charger();
-        System.out.println(json.ObtenirInfoGen("cycle"));
+    }
 
+    private static void charger(DeclarationJSON json) {
+        try {
+            json.charger();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
