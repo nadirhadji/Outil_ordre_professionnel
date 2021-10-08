d8eb9e2cdae458ee1293f5a9c5ae496ca0cacd7f
Hadji, Nadir <hadji.nadir@courrier.uqam.ca>
2021-10-07 20:45:05 -0400

    Ajout de l'argument Activite dans les methodes qui en avait besoin

--- src/Service/ServiceMessages.java
+++ src/Service/ServiceMessages.java
@@ -1,56 +1,60 @@
-import java.net.SocketOption;
+package Service;
+
+import Entite.Activite;
+import Utils.Constantes;
 
 public class ServiceMessages {
 
-    public String messageErreurCycleInvalide () {
+    public static String messageErreurCycleInvalide () {
         String cycleInvalide = "Le cycle entré n'est pas valide, " +
-                "Le cycle doit être \"2020-2022\"\n";
+                "Le cycle doit être "+ Constantes.CYCLE_AUTORISEE +"\n";
         return cycleInvalide;
     }
 
-    public String messageErreurActiviteHorsDateReconnue() {
+    public static String messageErreurActiviteHorsDateReconnue(Activite activite) {
         String horsDate = " L'activité " + activite.obtenirCategorie() +
-              " n'a pas été complétée dans le cycle prévue \"2020-2022\"\n";
+              " n'a pas été complétée entre le" +
+                Constantes.DATE_DEBUT_ACTIVITE_AUTORISEE+ " et "+
+                Constantes.DATE_FIN_ACTIVITE_AUTORISEE;
         return horsDate;
     }
 
-    public String erreurMessageCategorieNonReconnue(){
+    public static String erreurMessageCategorieNonReconnue(Activite activite){
         String categorieNonReconnue = "L'activité "+
                 activite.obtenirCategorie() + " n'est pas dans une "+
                 "catégorie reconnue. Elle sera ignorée\n";
         return categorieNonReconnue;
     }
 
-    public String messageErreurHeuresTransfereInferieurA0(){
+    public static String messageErreurHeuresTransfereInferieurA0(){
         String heuresTransfereesNegatif = "Les heures "+
                 "transférées du cycle précédent ne peuvent pas être négatif\n";
         return heuresTransfereesNegatif;
     }
 
-    public String messageInfosHeuresTransfereSuperieurA7(){
+    public static String messageInfosHeuresTransfereSuperieurA7(){
         String tropDHeuresTransferees = "Les heures transférées du cycle "+
              "précédent dépassent 7. Uniquement 7 heures seront considérées\n";
         return  tropDHeuresTransferees;
     }
     //TODO mettre le bon NbHeures
-    public String messageNombreHeuresTotalMoinsDe40(){
+    public static String messageNombreHeuresTotalMoinsDe40() {
         String peuDHeuresTotal= "Incomplet, il manque + (40heures - NbHeures) " +
                         "pour compléter le cycle\n";
         return peuDHeuresTotal;
     }
 
-    public String messageErreurNombreHeuresInsuffisantDansCategorie(){
+    public static String messageErreurNombreHeuresInsuffisantDansCategorie(){
         String nbrHeuresInsuffisantDansCategorie = "Le nombre d'Heures "+
                 "déclarées dans les catégories: cours, atelier, séminaire,"+
                 " colloque, conférence, lecture dirigée n'atteint pas 17\n";
         return nbrHeuresInsuffisantDansCategorie;
     }
 
-    public String messageErreurNombreHeuresPourActiviteInvalide(){
+    public static String messageErreurNombreHeuresPourActiviteInvalide(Activite activite){
         String nbrHeuresInvalide = "Le nombre d'heures entré pour " +
-                activite.obtenirCategorie()) + " est invalide, il doit être"+
+                activite.obtenirCategorie() + " est invalide, il doit être"+
                         "supérieur ou égal à 1. L'activité sera ignorée\n";
-
+        return nbrHeuresInvalide;
     }
-
 }
