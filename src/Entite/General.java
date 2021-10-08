5ec82c97faa5e8dd35795cac93eebb7db8d6bde8
Cishahayo, Steffie Morène <cishahayo.steffie_morene@courrier.uqam.ca>
2021-10-07 22:36:32 -0400

    modification+ajout commentaires

--- src/Entite/General.java
+++ src/Entite/General.java
@@ -3,6 +3,11 @@ package Entite;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * transforme les JSON object en String par des constructeurs pour qu'on puisse
+ * utiliser ces Strings tout au long du programme sans qu'on ne traduise chaque
+ * fois l'objet JSON
+ */
 public class General {
 
     private String numeroDePermis;
@@ -10,22 +15,38 @@ public class General {
     private int heuresTransfereesDuCyclePrecedent;
     private List<Activite> activites;
 
+    /**
+     * constructeur qui transforme le document JSON dans une liste d'activités
+     * @param declarationJson
+     */
     public General (DeclarationJSON declarationJson){
         this.activites = new ArrayList<>();
     }
 
+    /**
+     * @return this.numeroDePermis
+     */
     public String obtenirNumeroDePermis (){
         return this.numeroDePermis;
     }
 
+    /**
+     * @return this.cycle
+     */
     public String obtenirCycle (){
         return this.cycle;
     }
 
+    /**
+     * @return this.heuresTransfereesDuCyclePrecedent
+     */
     public int obtenirHeurestransfere (){
         return this.heuresTransfereesDuCyclePrecedent;
     }
 
+    /**
+     * @return this.activites
+     */
     public List<Activite> obtenirActivites (){
         return this.activites;
     }
@@ -34,10 +55,18 @@ public class General {
         heuresTransfereesDuCyclePrecedent = heuresTransfereesDuCyclePrecedent - nombre;
     }
 
+    /**
+     * transforme les heuresTransfereesDuCyclePrecedent a 7 chaque fois que
+     * l'utilisateur entre un nombre superieur a 7
+     */
     public void modifierNombreHeuresTransfereA7() {
         this.heuresTransfereesDuCyclePrecedent = 7;
     }
 
+    /**
+     * transforme les heuresTransfereesDuCyclePrecedent a 0 chaque fois que
+     * l'utilisateur entre un nombre inferieur a 0
+     */
     public void modifierNombreHeuresTransfereA0() {
         this.heuresTransfereesDuCyclePrecedent = 0;
     }
