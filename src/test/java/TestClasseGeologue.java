c6aeb4d605f090f136e748cda0299cfd274a3528
Felix Gonzalez <aa791038@ens.uqam.ca>
2021-11-10 12:51:07 -0500

    addition des testes pour chaque point avec des condition pour geologue

--- src/test/java/TestClasseGeologue.java
+++ src/test/java/TestClasseGeologue.java
@@ -1,9 +1,53 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestClasseGeologue {
-    @Test
-    public void cycleValide() {
 
+    @Disabled
+    @Test
+    public void cycleValide()
+    {
+        //TODO
     }
+
+    @Disabled
+    @Test
+    public void heureCoursValide()
+    {
+         //TODO
+    }
+
+
+    @Disabled
+    @Test
+    public void heureProjetRechercheValide()
+    {
+        //TODO
+    }
+
+
+    @Disabled
+    @Test
+    public void heureGroupeDiscussionValide()
+    {
+        //TODO
+    }
+
+    @Disabled
+    @Test
+    public void heureTotalValide()
+    {
+        //TODO
+    }
+
+    @Disabled
+    @Test
+    public void champsHeuresTransfereNonExistant()
+    {
+        //TODO
+    }
+
 
 }
