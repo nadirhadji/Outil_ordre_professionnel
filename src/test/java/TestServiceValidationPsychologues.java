185804adaa4093040fdd0524e9a8c3af10ad3a0e
Cishahayo, Steffie Morène <gh391909@ens.uqam.ca>
2021-11-11 12:19:07 -0500

    Template des tests pour Psychologues et Service message

---
+++ src/test/java/TestServiceValidationPsychologues.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestServiceValidationPsychologues {
+
+    @BeforeEach
+    void initialiser() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void verifier() {
+    }
+
+    @Test
+    void verifierCyclePsychologue() {
+    }
+
+    @Test
+    void estCyclePsychologueValide() {
+    }
+}
