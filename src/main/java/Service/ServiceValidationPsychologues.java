4fae4a2524f24e689ec92f5c2b303ad782beddbe
Cishahayo, Steffie Morène <gh391909@ens.uqam.ca>
2021-11-14 21:21:17 -0500

    ajout des getters des heures de cours et conference

--- src/main/java/Service/ServiceValidationPsychologues.java
+++ src/main/java/Service/ServiceValidationPsychologues.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class ServiceValidationPsychologues implements InterfaceVerification {
 
     private int heuresConference;
@@ -23,7 +24,7 @@ public class ServiceValidationPsychologues implements InterfaceVerification {
     public void verifier(Declaration general) {
         if ( verifierCyclePsychologue( general) ) {
             verifierActivites(general);
-            verifierNombreHeuresMinimumCours(heuresCours);
+            verifierNombreHeuresMinimumCours();
             verifierNombreHeuresPourConferenceSupA15(heuresConference);
             verifierNombreHeuresTotaleDansDeclaration();
         }
@@ -38,21 +39,12 @@ public class ServiceValidationPsychologues implements InterfaceVerification {
         }
         return true;
     }
-
     public boolean estCyclePsychologueValide( String cycle ) {
         return cycle.equals(Constantes.CYCLE_AUTORISE_POUR_PSYCHOLOGUES);
     }
 
-    private boolean estDateValidePourPsychologue(String date) {
-        boolean resultat;
-        if ( LocalDate.parse(date).isBefore(DATE_DEBUT_ACTIVITE_PSYCHO) )
-            resultat = false;
-        else
-            resultat = !LocalDate.parse(date).isAfter(DATE_FIN_ACTIVITE_PSYCHO);
-        return resultat;
-    }
 
-    private void verifierActivites(Declaration declaration) {
+    public void verifierActivites(Declaration declaration) {
         ServiceValidationActivite serviceValidationActivite = new
                 ServiceValidationActivite(declaration.obtenirOrdre(),declaration.obtenirCycle());
 
@@ -63,7 +55,7 @@ public class ServiceValidationPsychologues implements InterfaceVerification {
         }
     }
 
-    private void incrementerCompteurHeures(Activite activite) {
+    public void incrementerCompteurHeures(Activite activite) {
         String categorie = activite.obtenirCategorie();
         int nbrHeures = activite.obtenirHeures();
 
@@ -72,12 +64,12 @@ public class ServiceValidationPsychologues implements InterfaceVerification {
         verifierNombreHeuresConference(categorie,nbrHeures);
     }
 
-    private void verifierAutreCategories(String categorie, int nbrHeures) {
+    public void verifierAutreCategories(String categorie, int nbrHeures) {
         List<String> autreCategorie = obtenirAutresActivites();
         if(autreCategorie.contains(categorie))
             this.heuresAutreActivitesPsy += nbrHeures;
     }
-    private List<String> obtenirAutresActivites(){
+    public List<String> obtenirAutresActivites(){
         List<String> autreActivitePsychologue = new ArrayList<>();
 
         autreActivitePsychologue.add(Categorie.ATELIER.toString());
@@ -92,30 +84,38 @@ public class ServiceValidationPsychologues implements InterfaceVerification {
         return autreActivitePsychologue;
     }
 
-    private void verifierNombreHeuresCours(String categorie,int nbrHeure) {
+    public void verifierNombreHeuresCours(String categorie,int nbrHeure) {
         if ( categorie.equals(Categorie.COURS.toString()))
             this.heuresCours += nbrHeure;
     }
 
-    private void verifierNombreHeuresMinimumCours(int heuresCours){
+    public int obtenirHeuresCoursPsycho(){
+        return heuresCours;
+    }
+
+    public void verifierNombreHeuresMinimumCours(){
         if(this.heuresCours < ConstantesPsychologues.MINIMUM_HEURE_COURS){
             Reponse.obtenirInstance().ajouterMessageErreur(
                     ServiceMessages.messageErreurNombreHeuresPourCours());
         }
     }
 
-    private void verifierNombreHeuresConference(String categorie,int nbrHeure) {
+    public void verifierNombreHeuresConference(String categorie,int nbrHeure) {
         if ( categorie.equals(Categorie.CONFERENCE.toString()))
             this.heuresConference += nbrHeure;
     }
 
-    private void verifierNombreHeuresPourConferenceSupA15(int heuresConference){
+    public int verifierNombreHeuresPourConferenceSupA15(int heuresConference){
         if (heuresConference > ConstantesPsychologues.MAXIMUM_HEURE_CONFERENCE){
             this.heuresConference = 15;
         }
+        return this.heuresConference;
+    }
+    public int obtenirHeuresConference(){
+        return heuresConference;
     }
 
-    private void verifierNombreHeuresTotaleDansDeclaration() {
+    public void verifierNombreHeuresTotaleDansDeclaration() {
         int nombreHeuresManquante = obtenirNombreHeuresManquante();
         if( nombreHeuresManquante > 0 ) {
             Reponse.obtenirInstance().ajouterMessageErreur(
@@ -124,7 +124,7 @@ public class ServiceValidationPsychologues implements InterfaceVerification {
         }
     }
 
-    private int obtenirNombreHeuresManquante() {
+    public int obtenirNombreHeuresManquante() {
         int total = obtenirNombreTotalHeures();
 
         if ( total >= MINIMUM_HEURE_POUR_PSYCH)
@@ -133,7 +133,7 @@ public class ServiceValidationPsychologues implements InterfaceVerification {
             return MINIMUM_HEURE_POUR_PSYCH - total;
     }
 
-    private int obtenirNombreTotalHeures() {
+    public int obtenirNombreTotalHeures() {
         return heuresConference + heuresCours + heuresAutreActivitesPsy;
     }
 }
