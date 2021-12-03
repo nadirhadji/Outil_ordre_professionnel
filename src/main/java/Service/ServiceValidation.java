1c8c230e86531a747897a83d24377d7011aa91fe
Hadji, Nadir <hadji.nadir@courrier.uqam.ca>
2021-12-03 01:41:12 -0500

    Correction d'un bug

--- src/main/java/Service/ServiceValidation.java
+++ src/main/java/Service/ServiceValidation.java
@@ -6,8 +6,6 @@ public class ServiceValidation {
 
     public void validerDeclaration(Declaration declaration) {
         String ordre = declaration.obtenirOrdre();
-        String numeroDePermis = declaration.obtenirNumeroDePermis();
-        ServiceValidationNumeroDePermis.validerNumeroDePermis(numeroDePermis,ordre);
         InterfaceVerification interfaceVerification = VerificationFactory.obtenirInstance(ordre);
         interfaceVerification.verifier(declaration);
     }
