d28c0165f92fc79a03bf2abe9b4ca286b2790991
Cishahayo, Steffie Morène <gh391909@ens.uqam.ca>
2021-12-07 14:04:39 -0500

    ajout du regex du cycle psychologue

--- src/main/java/Utils/RegexNumeroDePermis.java
+++ src/main/java/Utils/RegexNumeroDePermis.java
@@ -5,5 +5,5 @@ public class RegexNumeroDePermis {
     public static final String ARCHITECTE = "^(A|T)[0-9]{4}$";
     public static final String GEOLOGUE = "[A-Z]{2}[0-9]{4}";
     public static final String PODIATRE = "[0-9]{5}";
-    public static final String PSHYCOLOGUE = "";
+    public static final String PSHYCOLOGUE = "[0-9]{5}-[0-9]{2}";
 }
